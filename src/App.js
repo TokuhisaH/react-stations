c0da41affe4e7dba5d479fbc987a7c7b3cc3ec77
TokuhisaH <tokuhisahiroki@gmail.com>
2021-08-19 19:31:09 +0900

    use useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React,{useState} from 'react';
 import './App.css'
 
 /**
  * 
  * @type {React.FC}
  */
-export const App = () => {
+export const App = (props) => {
+
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/sheepdog-english/n02105641_3499.jpg");
+
+
   return (
     <React.Fragment>  
       <header>Hello アプリ</header>
       <h2>犬の画像を表示するためのアプリです</h2>
-      <img src="https://images.dog.ceo/breeds/sheepdog-english/n02105641_3499.jpg"></img>
+      <img src={dogUrl}></img>
     </React.Fragment>
   )
 }
