9ad03c08341c440f56d6719522ebf697a9577d9c
TokuhisaH <tokuhisahiroki@gmail.com>
2021-08-28 17:24:25 +0900

    station10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,31 @@
 // DO NOT DELETE
+import React,{useState,useEffect} from 'react';
+import { DogListContainer } from './DogListContainer';
+
+export const BreedsSelect = (props) =>{
+    
+    const breeds =props.breeds;
+    console.log(breeds)
+
+
+    const breedslist = breeds.map((breeds)=>
+        <option value={breeds}>{breeds}</option>
+    );
+
+    const handleChange = () =>{
+        const ele = document.getElementById( "pulldownBreed" ) ;
+        setSelectedBreed(ele.value)
+    }
+
+    const[selectedBreed,setSelectedBreed]= useState()
+    console.log("今選ばれてる",selectedBreed)
+
+    return　(
+        <React.Fragment>
+            <select id="pulldownBreed" onChange={handleChange}>
+                <option value="">-</option>
+                {breedslist}
+            </select>
+        </React.Fragment>
+    )
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,19 +1,24 @@
 // DO NOT DELETE
 import React,{useState,useEffect} from 'react';
+import { BreedsSelect } from './BreedsSelect';
 
 export const DogListContainer = (props) =>{
 
+    // APIで拾ってきた犬種リスト
     const [breeds, setBreeds] = useState([]);
+
+    // プルダウンで選択した犬
+    const[selectedBreed,setSelectedBreed]= useState()
   
     useEffect (() =>{
       fetch("https://dog.ceo/api/breeds/list/all")
       .then(res => res.json())
       .then(data => setBreeds(data.message))
-    });
+    },[]);
 
     return (
         <React.Fragment>
+            <BreedsSelect breeds={Object.keys(breeds)} />
         </React.Fragment>
     );
-
 }
